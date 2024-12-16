f2726f585f941f6496039a21993a117087f425ab
heaplax <1837035764lwj@gmail.com>
2024-12-16 14:20:57 -0500

    move with ship

--- Assets/Scripts/Controllers/Ship.cs
+++ Assets/Scripts/Controllers/Ship.cs
@@ -81,7 +81,7 @@ public class Ship : GravityObject {
                 shipCam.enabled = false;
                 mapCamObject = new GameObject("Map Camera");
                 CameraBehavior cameraBehavior = mapCamObject.AddComponent<CameraBehavior>();
-                cameraBehavior.Initialize(motionData.Position);
+                cameraBehavior.Initialize(this.transform);
                 
             }
             else
--- Assets/Scripts/Game/CameraBehavior.cs
+++ Assets/Scripts/Game/CameraBehavior.cs
@@ -8,7 +8,7 @@ public class CameraBehavior : MonoBehaviour
 {
     public const float zoomSpeed = 1f; // 缩放速度
     public const float moveSpeed = 1f; // WSAD移动速度
-    public const float minZoom = 5f;   // 缩放的最小值
+    public const float minZoom = 1f;   // 缩放的最小值
     public const float maxZoom = 5000f;  // 缩放的最大值
 
 
@@ -20,18 +20,21 @@ public class CameraBehavior : MonoBehaviour
     GameObject mapCamObject;
 
 
-    public List<GameObject> labelList;
+    public List<GameObject> labelList, labelList2;
 
     public Font textFont;
 
     // 构造函数，接收一个 position 参数，用于初始化摄像机
-    public void Initialize(Vector3 position)
+    public void Initialize(Transform cameraBase)
     {
         mapCamObject = this.transform.gameObject;
+        
+        Debug.Log(cameraBase.position);
+
         mapCam = mapCamObject.AddComponent<Camera>();
 
         // 设置摄像机的位置
-        mapCam.transform.position = position + new Vector3(0, 0, -2500);
+        mapCam.transform.position = cameraBase.position + new Vector3(0, 0, -2500);
 
         // 设置摄像机的旋转方向，使其视角为 z 轴向下
         mapCam.transform.rotation = Quaternion.Euler(0, 0, 0);
@@ -45,9 +48,13 @@ public class CameraBehavior : MonoBehaviour
         // 设置摄像机的标签为 MainCamera
         mapCam.tag = "MainCamera";
 
+
+        this.transform.SetParent(cameraBase);
+
         labelList = new List<GameObject>();
 
         GameObject[] planets = GameObject.FindGameObjectsWithTag("Planet");
+
         foreach (GameObject planet in planets)
         {
             GameObject textObject = new GameObject(planet.name + "_Label");
@@ -69,6 +76,34 @@ public class CameraBehavior : MonoBehaviour
             rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(planet.transform.position);
 
             labelList.Add(textObject);
+
+        }
+
+        labelList2 = new List<GameObject>();
+
+        GameObject[] ships = GameObject.FindGameObjectsWithTag("Ship");
+
+        foreach (GameObject ship in ships)
+        {
+            GameObject textObject = new GameObject(ship.name + "_Label");
+            textObject.transform.SetParent(GameObject.Find("UI").transform);
+
+            // 添加Text组件
+            Text textComponent = textObject.AddComponent<Text>();
+            textComponent.text = "Ship";
+            textComponent.fontSize = 24;
+            textFont = Resources.Load<Font>("Consolas");
+            textComponent.font = textFont;
+            textComponent.color = Color.green;
+            textComponent.alignment = TextAnchor.MiddleCenter;
+            Outline outline = textComponent.gameObject.AddComponent<Outline>();
+            outline.effectColor = Color.white;
+
+            // 设置Text对象的位置
+            RectTransform rectTransform = textObject.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(ship.transform.position);
+
+            labelList2.Add(textObject);
 
         }
 
@@ -95,19 +130,21 @@ public class CameraBehavior : MonoBehaviour
 
                 LineRendererHandler.setWidthMap(camera.orthographicSize);
 
+
+                
             }
 
             // WSAD�ƶ�
             float horizontal = Input.GetAxis("Horizontal"); // A/D �� ��/�ҷ����
             float vertical = Input.GetAxis("Vertical");     // W/S �� ��/�·����
 
-            Vector3 move = new Vector3(horizontal, vertical, 0) * moveSpeed * camera.orthographicSize * Time.deltaTime;
-            camera.transform.Translate(move, Space.World);
+            if (horizontal != 0.0f || vertical != 0.0f)
+            {
+                Vector3 move = new Vector3(horizontal, vertical, 0) * moveSpeed * camera.orthographicSize * Time.deltaTime;
+                camera.transform.Translate(move, Space.World);
+            }
 
 
-
-            // Label
-            //foreach (Text child in labels.GetComponentsInChildren<Text>())
             foreach (GameObject child in labelList)
             {
                 //Debug.Log("update");
@@ -130,14 +167,36 @@ public class CameraBehavior : MonoBehaviour
                     child.SetActive(camera.orthographicSize >= body.radius * hideCoefficient);
                 }
 
-                
+
             }
+
+
+            foreach (GameObject child in labelList2)
+            {
+                //Debug.Log("update");
+                GameObject ship = GameObject.Find(child.name.Replace("_Label", ""));
+                if (ship != null)
+                {
+                    RectTransform rectTransform = child.GetComponent<RectTransform>();
+                    Vector3 vector = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f);
+                    rectTransform.anchoredPosition = Camera.main.WorldToScreenPoint(ship.transform.position) - vector;
+                }
+
+                child.SetActive(camera.orthographicSize > 5);
+
+            }
+
         }
     }
 
     void OnDestroy()
     {
         foreach (GameObject child in labelList)
+        {
+            Destroy(child);
+        }
+
+        foreach (GameObject child in labelList2)
         {
             Destroy(child);
         }
